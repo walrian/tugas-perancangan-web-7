e1e4a433910ac09d7a55b05a2d4af49203e43377
unknown <walriansihombing@gmail.com>
2025-05-15 20:57:03 +0700

    first commit

---
+++ soal 1.js
@@ -0,0 +1,11 @@
+// Problem 1 - String Concatenation
+let firstName = 'walrian';
+let LastName = 'sihombing';
+let bornYear = 2005;
+
+// Calculate age based on the current year (2023)
+let currentYear = 2025;
+let age = currentYear - bornYear;
+
+// Display the personalized message
+console.log(`Hello, ${firstName} ${LastName}! You are ${age} years old.`);
