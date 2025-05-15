7e3f66b0d1073dce5f4e470043705fa810340461
WALRIAN SIHOMBING <walriansihombing@gmail.com>
2025-05-15 21:13:50 +0700

    soal 3.js

---
+++ soal 3.js
@@ -0,0 +1,10 @@
+let panjang = 20;
+let lebar = 5;
+
+// Gunakan rumus luas persegi panjang: panjang * lebar
+let luas = panjang * lebar;
+console.log('Luas persegi Panjang = ' + luas);
+
+// Gunakan rumus keliling persegi panjang: 2 * (panjang + lebar)
+let keliling = 2 * (panjang + lebar);
+console.log('Keliling persegi Panjang = ' + keliling);
